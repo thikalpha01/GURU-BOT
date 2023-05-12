b322d8e0db11774d558cc98e73ae934817954c90
thikalpha01 <126881740+thikalpha01@users.noreply.github.com>
2023-05-12 13:58:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['94762491926', 'alpha', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
